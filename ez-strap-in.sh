d4930bfb96d67c462c41f6101184be61b70dd113
elias walker <eliasw4u@gmail.com>
2018-06-10 22:02:32 +0000

    Update ez-strap-in.sh

--- ez-strap-in.sh
+++ ez-strap-in.sh
@@ -4,7 +4,11 @@
 # mounted to /mnt and ready to be writen to. (assumes you are loged in as root) 
 # and you must be in the folder that you are running it from. because it
 # depends on other sctipts to finnish (needs install-me, .zshrc, .bashrc, arch1, and install-help)
+zenity --question --height=50 --width=450 --title="$title" --text "Welcome to the Arrow Installer.\n\nIt is necessary\nto have the drive you want\nArrow installed on mounted at\n /mnt. \nthis will faill if it is not.\nAre you ready to begin?\n\nClick 'yes' to begin or 'no' to exit."
 
+if [ "$?" = "1" ]
+	then exit
+fi
 mount -o remount,size=1G /run/archiso/cowspace
 pacstrap /mnt base base-devel
 genfstab -U -p /mnt > /mnt/etc/fstab && cat /mnt/etc/fstab
@@ -13,6 +17,7 @@ genfstab -U -p /mnt > /mnt/etc/fstab && cat /mnt/etc/fstab
 cp /home/liveuser/Desktop/ez-install-script/arrow1.sh /mnt
 cp /home/liveuser/Desktop/ez-install-script/install-yaourt.sh /mnt/root
 cp /home/liveuser/Desktop/ez-install-script/ez-install.sh /mnt/root
+cp /home/liveuser/Desktop/ez-install-script/ez-install.sh /mnt
 
 cp -R /home/liveuser/Desktop/ez-install-script /mnt/root
 
@@ -33,7 +38,7 @@ cp -R /home/liveuser/.themes /mnt/root
 cp -R /home/liveuser/Desktop /mnt/root
 cp -R /home/liveuser/Pictures /mnt/root
 cp -R /home/liveuser/icons /mnt/root
-
+cp -R /usr/share/my-icons /mnt/usr/share
 
 
 
